37d4892d69e6ded12fbb8fac1df0b7a54e06eb8e
Javier Perez <javierpdc13@hotmail.com>
2022-06-20 15:12:04 -0400

    Add entities and App context

---
+++ E-market.Core.Domain/Entities/Article.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_market.Core.Domain.Entities
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImgUrl { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+        public int UserId { get; set; }
+
+        //Navigation Properties
+        public Category Category { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ E-market.Infrastructure.Persistence/Contexts/ApplicationContext.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_market.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_market.Infrastructure.Persistence.Contexts
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) {  }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region tables
+            modelBuilder.Entity<Article>().ToTable("Articles");
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<Category>().ToTable("Categories");
+            #endregion
+
+            #region "primary keys"
+            modelBuilder.Entity<Article>().HasKey(h => h.Id);
+            modelBuilder.Entity<User>().HasKey(h => h.Id);
+            modelBuilder.Entity<Category>().HasKey(h => h.Id);
+            #endregion
+
+            #region "Relationships"
+            modelBuilder.Entity<Category>()
+                .HasMany<Article>(s => s.Articles)
+                .WithOne(g => g.Category)
+                .HasForeignKey(s => s.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .HasMany<Article>(s => s.Articles)
+                .WithOne(g => g.User)
+                .HasForeignKey(s => s.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+            #endregion
+
+            #region Configurations
+            #region Articles
+            modelBuilder.Entity<Article>()
+                .Property(article => article.Name)
+                .IsRequired()
+                .HasMaxLength(75);
+
+            modelBuilder.Entity<Article>()
+                .Property(article => article.ImgUrl)
+                .IsRequired();
+
+            modelBuilder.Entity<Article>()
+                .Property(article => article.Price)
+                .IsRequired();
+
+            modelBuilder.Entity<Article>()
+                .Property(article => article.Description)
+                .IsRequired();
+            #endregion
+
+            #region Categories
+            modelBuilder.Entity<Category>()
+                .Property(category => category.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Category>()
+                .Property(category => category.Description)
+                .IsRequired();
+            #endregion
+
+            #region Users
+            modelBuilder.Entity<User>()
+                .Property(user => user.Name)
+                .IsRequired()
+                .HasMaxLength(35);
+
+            modelBuilder.Entity<User>()
+                .Property(user => user.LastName)
+                .IsRequired()
+                .HasMaxLength(35);
+
+            modelBuilder.Entity<User>()
+                .Property(user => user.Email)
+                .IsRequired();
+
+            modelBuilder.Entity<User>()
+                .Property(user => user.Phone)
+                .IsRequired();
+
+            modelBuilder.Entity<User>()
+                .Property(user => user.UserName)
+                .IsRequired();
+
+            modelBuilder.Entity<User>()
+                .Property(user => user.Password)
+                .IsRequired();
+            #endregion
+            #endregion
+        }
+    }
+}
