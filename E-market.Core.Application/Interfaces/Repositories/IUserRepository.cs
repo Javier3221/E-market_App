0e3e78d7503571a17660d51fb7ba690c750a492a
Javier Perez <javierpdc13@hotmail.com>
2022-06-21 19:27:14 -0400

    Change namespaces to User and article interfaces

---
+++ E-market.Core.Application/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using E_market.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_market.Core.Application.Interfaces.Repositories
+{
+    public interface IUserRepository : IGenericRepositoryAsync<User>
+    {
+
+    }
+}
