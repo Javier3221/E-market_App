ce51a900c3330699e711d28a8176fec466067d1e
Javier Perez <javierpdc13@hotmail.com>
2022-06-20 21:41:08 -0400

    Change Application context from startup to Persistence

---
+++ E-market.Infrastructure.Persistence/ServiceRegistration.cs
@@ -0,0 +1,38 @@
+﻿using E_market.Core.Application.Interfaces;
+using E_market.Infrastructure.Persistence.Contexts;
+using E_market.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_market.Infrastructure.Persistence
+{
+    //Extension Methods - Decorator pattern
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Context
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<ApplicationContext>(options => options.UseInMemoryDatabase("ApplicationDb"));
+            }
+            else
+            {
+                services.AddDbContext<ApplicationContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                    m => m.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName)));
+            }
+            #endregion
+
+            #region Repositories
+            services.AddTransient<IUserRepository, UserRepository>();
+            #endregion
+        }
+    }
+}
--- E-market_OnionMVC/Startup.cs
+++ E-market_OnionMVC/Startup.cs
@@ -1,13 +1,10 @@
+using E_market.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace E_market_OnionMVC
 {
@@ -23,6 +20,8 @@ namespace E_market_OnionMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddPersistenceInfrastructure(Configuration);
+
             services.AddControllersWithViews();
         }
 
@@ -50,7 +49,7 @@ namespace E_market_OnionMVC
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
