c7bcfaeda1489a47a2d3d81448ad5a73ca68dcd9
Javier Perez <javierpdc13@hotmail.com>
2022-06-21 18:56:45 -0400

    Create GenericRepository.cs

---
+++ E-market.Infrastructure.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_market.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_market.Infrastructure.Persistence.Repositories
+{
+    public class GenericRepository<Entity> where Entity : class
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public GenericRepository(ApplicationContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(Entity entity)
+        {
+            await _dbContext.Set<Entity>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Entity entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Entity entity)
+        {
+            _dbContext.Set<Entity>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<Entity>> GetAllAsync()
+        {
+            return await _dbContext.Set<Entity>().ToListAsync();
+        }
+
+        public async Task<Entity> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<Entity>().FindAsync(id);
+        }
+    }
+}
