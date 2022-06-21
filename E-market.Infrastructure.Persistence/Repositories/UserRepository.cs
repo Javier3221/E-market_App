cb56e744b99a8c09af5c0bb4b62db31307631444
Javier Perez <javierpdc13@hotmail.com>
2022-06-20 21:40:02 -0400

    Add user and article repository

---
+++ E-market.Infrastructure.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using E_market.Infrastructure.Persistence.Contexts;
+using E_market.Core.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_market.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public UserRepository(ApplicationContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
