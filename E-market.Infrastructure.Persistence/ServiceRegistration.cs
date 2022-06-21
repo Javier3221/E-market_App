10d884bf33f3cc2aeda58193ef6ab0f564a0fda9
Javier Perez <javierpdc13@hotmail.com>
2022-06-21 18:57:14 -0400

    Delete unused namespaces

--- E-market.Infrastructure.Persistence/ServiceRegistration.cs
+++ E-market.Infrastructure.Persistence/ServiceRegistration.cs
@@ -4,11 +4,6 @@ using E_market.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace E_market.Infrastructure.Persistence
 {
