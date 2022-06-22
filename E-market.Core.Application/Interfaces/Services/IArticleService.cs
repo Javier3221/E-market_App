19fb9b86f7cea33cf03f7b552cc7718dc45d8501
Javier Perez <javierpdc13@hotmail.com>
2022-06-22 13:25:26 -0400

    Add service interfaces for all CRUDs

---
+++ E-market.Core.Application/Interfaces/Services/IArticleService.cs
@@ -0,0 +1,14 @@
+﻿using E_market.Core.Application.ViewModels.Articles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_market.Core.Application.Interfaces.Services
+{
+    public interface IArticleService : IGenericService<SaveArticleViewModel, GetArticleViewModel>
+    {
+        
+    }
+}
---
+++ E-market.Core.Application/Interfaces/Services/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_market.Core.Application.ViewModels.Categories;
+
+namespace E_market.Core.Application.Interfaces.Services
+{
+    public interface ICategoryService : IGenericService<CategoryViewModel, CategoryViewModel>
+    {
+
+    }
+}
