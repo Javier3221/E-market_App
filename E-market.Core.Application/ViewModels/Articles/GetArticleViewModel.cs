d2fc32aef66aae1de13f4ae2764dbf93995b7be9
Javier Perez <javierpdc13@hotmail.com>
2022-06-22 13:27:46 -0400

    Add a new view model for article
    this one has strings instead of IDs, for easier implementation

---
+++ E-market.Core.Application/ViewModels/Articles/GetArticleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_market.Core.Application.ViewModels.Articles
+{
+    public class GetArticleViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImgUrl { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string UserName { get; set; }
+    }
+}
