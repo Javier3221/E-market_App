dda4ad0e63d0cae19b73781aa1bd98da0267273c
Javier Perez <javierpdc13@hotmail.com>
2022-06-22 13:29:24 -0400

    Create CategoryRepository.cs

---
+++ E-market.Infrastructure.Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using E_market.Infrastructure.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_market.Core.Domain.Entities;
+using E_market.Core.Application.Interfaces.Repositories;
+
+namespace E_market.Infrastructure.Persistence.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public CategoryRepository(ApplicationContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
