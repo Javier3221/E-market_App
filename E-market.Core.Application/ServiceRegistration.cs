dce06241257f031c4ceb58c8f9161dae1f1d6c0b
Javier Perez <javierpdc13@hotmail.com>
2022-06-22 13:25:48 -0400

    Add dependency injection to services

---
+++ E-market.Core.Application/ServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using E_market.Core.Application.Interfaces.Services;
+using E_market.Core.Application.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_market.Core.Application
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Services
+            services.AddTransient<IArticleService, ArticleService>();
+            services.AddTransient<ICategoryService, CategoryService>();
+            services.AddTransient<IUserService, UserService>();
+            #endregion
+        }
+    }
+}
