f6fe04ec66bce9c5c69f09d74ed3e7fd2d3b078d
Javier Perez <javierpdc13@hotmail.com>
2022-06-22 13:24:53 -0400

    Create generic service interface for service interfaces

---
+++ E-market.Core.Application/Interfaces/Services/IGenericService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_market.Core.Application.Interfaces.Services
+{
+    public interface IGenericService<SaveViewModel, GetViewModel>
+        where SaveViewModel : class
+        where GetViewModel : class
+    {
+        Task Add(SaveViewModel vm);
+        Task Update(SaveViewModel vm);
+        Task Delete(int id);
+        Task<SaveViewModel> GetByIdSaveViewModel(int id);
+        Task<List<GetViewModel>> GetAllViewModel();
+    }
+}
