9533fbfb86739c00ed3b652d1e178df82e115c17
Javier Perez <javierpdc13@hotmail.com>
2022-06-22 22:29:58 -0400

    Add Data Annotations to categories

--- E-market.Core.Application/ViewModels/Categories/CategoryViewModel.cs
+++ E-market.Core.Application/ViewModels/Categories/CategoryViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace E_market.Core.Application.ViewModels.Categories
     public class CategoryViewModel
     {
         public int Id { get; set; }
+        [Required (ErrorMessage = "The category name is required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "You need to describe what is this category")]
         public string Description { get; set; }
         public int ArticleCount { get; set; }
     }
