f291d1756078f0b1006ae5de5e3f96f68d009ef4
Javier Perez <javierpdc13@hotmail.com>
2022-06-22 22:30:21 -0400

    Create UserArticlesController.cs

---
+++ E-market_OnionMVC/Controllers/UserArticlesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_market_OnionMVC.Controllers
+{
+    public class UserArticlesController : Controller
+    {
+        public IActionResult ArticleList()
+        {
+            return View();
+        }
+    }
+}
