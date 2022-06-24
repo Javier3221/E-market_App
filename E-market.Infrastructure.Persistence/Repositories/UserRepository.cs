517c5a591f72fefdae236e2e6a72f0fd80235106
Javier Perez <javierpdc13@hotmail.com>
2022-06-24 14:16:24 -0400

    Add password encryption for accounts

--- E-market.Infrastructure.Persistence/Repositories/UserRepository.cs
+++ E-market.Infrastructure.Persistence/Repositories/UserRepository.cs
@@ -1,11 +1,14 @@
 ﻿using E_market.Infrastructure.Persistence.Contexts;
 using E_market.Core.Application.Interfaces.Repositories;
+using E_market.Core.Application.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using E_market.Core.Domain.Entities;
+using E_market.Core.Application.ViewModels.Users;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_market.Infrastructure.Persistence.Repositories
 {
@@ -16,6 +19,12 @@ namespace E_market.Infrastructure.Persistence.Repositories
         public UserRepository(ApplicationContext dbContext) : base (dbContext)
         {
             _dbContext = dbContext;
+        }
+
+        public override async Task AddAsync(User entity)
+        {
+            entity.Password = PasswordEcryption.ComputeSha256Hash(entity.Password);
+            await base.AddAsync(entity);
         }
     }
 }
