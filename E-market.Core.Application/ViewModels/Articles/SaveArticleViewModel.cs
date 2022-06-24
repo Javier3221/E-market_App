9940083e573d8a75659370956d353459be095325
Javier Perez <javierpdc13@hotmail.com>
2022-06-24 10:01:52 -0400

    Add data annotations to article view model

--- E-market.Core.Application/ViewModels/Articles/SaveArticleViewModel.cs
+++ E-market.Core.Application/ViewModels/Articles/SaveArticleViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using E_market.Core.Application.ViewModels.Categories;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +11,17 @@ namespace E_market.Core.Application.ViewModels.Articles
     public class SaveArticleViewModel
     {
         public int Id { get; set; }
+        [Required (ErrorMessage = "This article needs a name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "You have to include at least one image")]
         public string ImgUrl { get; set; }
+        [Required(ErrorMessage = "You have to set the price for the article")]
         public double Price { get; set; }
+        [Required(ErrorMessage = "You have to describe the article")]
         public string Description { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "You have to set the category for the article")]
         public int CategoryId { get; set; }
         public int UserId { get; set; }
+        public List<CategoryViewModel> Categories { get; set; }
     }
 }
