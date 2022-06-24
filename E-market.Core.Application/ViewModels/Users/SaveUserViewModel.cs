0e3f89db090b97aafcf7630d854d9fa1e03a2acb
Javier Perez <javierpdc13@hotmail.com>
2022-06-24 14:18:48 -0400

    Create SaveUserViewModel.cs

---
+++ E-market.Core.Application/ViewModels/Users/SaveUserViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_market.Core.Application.ViewModels.Users
+{
+    public class SaveUserViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "You need to specify your name")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "You need to specify your last name")]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "You need to specify your email")]
+        [DataType(DataType.Text)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "You need to specify your phone number")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "You need to specify your user name")]
+        [DataType(DataType.Text)]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "You need to specify your password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password), ErrorMessage = "Passwords don't match")]
+        [Required(ErrorMessage = "You have to confirm your password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
