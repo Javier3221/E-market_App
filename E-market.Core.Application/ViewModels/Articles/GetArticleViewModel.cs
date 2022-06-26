26b0b7a9f2aa16e5021c9e87c8045bd237029776
Javier Perez <javierpdc13@hotmail.com>
2022-06-26 18:12:27 -0400

    Add prop to store files

--- E-market.Core.Application/ViewModels/Articles/GetArticleViewModel.cs
+++ E-market.Core.Application/ViewModels/Articles/GetArticleViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace E_market.Core.Application.ViewModels.Articles
         public string Description { get; set; }
         public string Category { get; set; }
         public string UserName { get; set; }
+        public List<IFormFile> Files { get; set; }
 
         public int CategoryId { get; set; }
     }
--- E-market.Core.Application/ViewModels/Articles/SaveArticleViewModel.cs
+++ E-market.Core.Application/ViewModels/Articles/SaveArticleViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using E_market.Core.Application.ViewModels.Categories;
+using E_market.Core.Application.Custom_Data_Annotations;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -12,15 +14,19 @@ namespace E_market.Core.Application.ViewModels.Articles
     {
         public int Id { get; set; }
         [Required (ErrorMessage = "This article needs a name")]
+        [DataType(DataType.Text)]
         public string Name { get; set; }
-        [Required(ErrorMessage = "You have to include at least one image")]
         public string ImgUrl { get; set; }
         [Required(ErrorMessage = "You have to set the price for the article")]
+        [DataType(DataType.Currency)]
         public double Price { get; set; }
         [Required(ErrorMessage = "You have to describe the article")]
         public string Description { get; set; }
         [Range(1, int.MaxValue, ErrorMessage = "You have to set the category for the article")]
         public int CategoryId { get; set; }
+        [DataType(DataType.Upload)]
+        [MaxRange4(ErrorMessage = "You can upload a maximum of 4 images")]
+        public List<IFormFile> Files { get; set; }
         public int UserId { get; set; }
         public List<CategoryViewModel> Categories { get; set; }
     }
