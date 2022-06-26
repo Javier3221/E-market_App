cbefe2077b1c3a7dacfbcd03c8305dae6d13d0ed
Javier Perez <javierpdc13@hotmail.com>
2022-06-26 18:13:41 -0400

    Update GenericRepository.cs

--- E-market.Infrastructure.Persistence/Repositories/GenericRepository.cs
+++ E-market.Infrastructure.Persistence/Repositories/GenericRepository.cs
@@ -15,10 +15,11 @@ namespace E_market.Infrastructure.Persistence.Repositories
             _dbContext = dbContext;
         }
 
-        public virtual async Task AddAsync(Entity entity)
+        public virtual async Task<Entity> AddAsync(Entity entity)
         {
             await _dbContext.Set<Entity>().AddAsync(entity);
             await _dbContext.SaveChangesAsync();
+            return entity;
         }
 
         public virtual async Task UpdateAsync(Entity entity)
