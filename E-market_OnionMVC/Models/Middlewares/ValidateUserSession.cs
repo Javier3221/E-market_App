c92b43798d3a0842fcf6cc9c284fa62ecf86a432
Javier Perez <javierpdc13@hotmail.com>
2022-06-25 22:53:44 -0400

    Create class for session validation

---
+++ E-market_OnionMVC/Models/Middlewares/ValidateUserSession.cs
@@ -0,0 +1,27 @@
+﻿using E_market.Core.Application.Helpers;
+using E_market.Core.Application.ViewModels.Users;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_market_OnionMVC.Models.Middlewares
+{
+    public class ValidateUserSession
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ValidateUserSession(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public bool HasUser()
+        {
+            UserViewModel userViewModel = _httpContextAccessor.HttpContext.Session.Get<UserViewModel>("user");
+
+            return userViewModel != null;
+        }
+    }
+}
