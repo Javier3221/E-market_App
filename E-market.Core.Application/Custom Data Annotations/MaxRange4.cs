23376cde3482e405b5e076f0d0c59bb9a0cf86ef
Javier Perez <javierpdc13@hotmail.com>
2022-06-26 18:08:40 -0400

    Create custom Data annotation
    used to validate if the user inserts more than 4 images

---
+++ E-market.Core.Application/Custom Data Annotations/MaxRange4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_market.Core.Application.Custom_Data_Annotations
+{
+    class MaxRange4 : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var list = value as IList;
+            if (list != null)
+            {
+                return list.Count <= 4;
+            }
+            return false;
+        }
+    }
+}
