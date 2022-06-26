855c44cec4645b97cf31d9e8b7b832a6e1bbffae
Javier Perez <javierpdc13@hotmail.com>
2022-06-25 22:54:21 -0400

    Enable sessions in startup

--- E-market_OnionMVC/Startup.cs
+++ E-market_OnionMVC/Startup.cs
@@ -5,7 +5,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.AspNetCore.Http;
+using E_market_OnionMVC.Models.Middlewares;
 
 namespace E_market_OnionMVC
 {
@@ -21,10 +22,13 @@ namespace E_market_OnionMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSession();
             services.AddPersistenceInfrastructure(Configuration);
-            services.AddApplicationLayer(Configuration);
+            services.AddApplicationLayer();
 
             services.AddControllersWithViews();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddTransient<ValidateUserSession, ValidateUserSession>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +44,7 @@ namespace E_market_OnionMVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseSession();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
